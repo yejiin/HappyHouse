911712bfc9015de509055aa9fd7955d4eaa46ffb
sewon <hwangsw369@naver.com>
2021-11-20 14:58:44 +0900

    feat: user 추가

--- HappyHouse_FrontEnd/src/router/index.js
+++ HappyHouse_FrontEnd/src/router/index.js
@@ -2,7 +2,33 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 
-Vue.use(VueRouter)
+import Member from "@/views/Member.vue";
+import MemberLogin from "@/components/user/MemberLogin.vue";
+import MemberJoin from "@/components/user/MemberJoin.vue";
+import MemberMyPage from "@/components/user/MemberMyPage.vue";
+
+import store from "@/store/index.js";
+
+Vue.use(VueRouter);
+
+// https://router.vuejs.org/kr/guide/advanced/navigation-guards.html
+const onlyAuthUser = async (to, from, next) => {
+  // console.log(store);
+  const checkUserInfo = store.getters["memberStore/checkUserInfo"];
+  const getUserInfo = store._actions["memberStore/getUserInfo"];
+  let token = sessionStorage.getItem("access-token");
+  if (checkUserInfo == null && token) {
+    await getUserInfo(token);
+  }
+  if (checkUserInfo === null) {
+    alert("로그인이 필요한 페이지입니다..");
+    // next({ name: "SignIn" });
+    router.push({ name: "SignIn" });
+  } else {
+    console.log("로그인 했다.");
+    next();
+  }
+};
 
 const routes = [
   {
@@ -11,13 +37,33 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path: "/user",
+    name: "Member",
+    component: Member,
+    children: [
+      {
+        path: "singin",
+        name: "SignIn",
+        component: MemberLogin,
+      },
+      {
+        path: "singup",
+        name: "SignUp",
+        component: MemberJoin,
+      },
+      {
+        path: "mypage",
+        name: "MyPage",
+        beforeEnter: onlyAuthUser,
+        component: MemberMyPage,
+      },
+    ],
+  },
+  {
+    path: "*",
+    redirect: "/",
+  },
+  
 ]
 
 const router = new VueRouter({
